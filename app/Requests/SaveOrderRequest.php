791869e7347fb3d35ac242a074103f7b6898647e
Куаныш <zheksenkulov.kuanysh@gmail.com>
2019-10-10 13:57:26 +0600

    10.10.2019 13:57

--- app/Requests/SaveOrderRequest.php
+++ app/Requests/SaveOrderRequest.php
@@ -13,10 +13,10 @@ class SaveOrderRequest extends FormRequest
             'order.id'                  => $order['id']      ? 'exists:orders,id' : 'nullable',
             'order.user_id'             => $order['user_id'] ? 'exists:users,id'  : 'nullable',
             'order.status_id'           => 'exists:order_statuses,id',
-            'order.carrier_id'          => 'exists:carriers,id',
+            'order.carrier_id'          => ($order['carrier_id'] ? 'exists:carriers,id' : 'nullable'),
             'order.comment'             => 'max:500',
             'order.delivery_date'       => 'nullable|date_format:"Y-m-d H:i:s"',
-            'order.payment_id'          => 'exists:payments,id',
+            'order.payment_id'          => ($order['payment_id'] ? 'exists:payments,id' : 'nullable'),
             'order.paid'                => 'integer|min:0|max:1',
             'order.payment_date'        => 'nullable|date_format:"Y-m-d H:i:s"',
             'order.created_at'          => 'required|date_format:"Y-m-d H:i:s"',
@@ -29,6 +29,8 @@ class SaveOrderRequest extends FormRequest
 
             'order.products'            => 'required'
         ];
+
+
 
         return $rules;
     }
